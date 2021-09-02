26be6eaf4b36c2c7448055cc7bde47cc679f8446
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-09-02 07:33:57 +0530

    angular error informer is bad

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Todo } from 'src/app/models/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  todos: Todo[];
+  constructor() {
+    this.todos = []
+  }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        content: "do internship",
+        completed: true
+      },
+      {
+        content: "get job",
+        completed: false
+      }
+    ]
   }
 
 }
