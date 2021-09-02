352c31397268e68dfb8f997605e0d626762b6cda
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-09-02 08:26:50 +0530

    completed

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -5,8 +5,10 @@ import { Todo } from 'src/app/models/Todo';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
+
 export class TodosComponent implements OnInit {
   todos: Todo[];
+  inputTodo: string = "";
   constructor() {
     this.todos = []
   }
@@ -14,14 +16,34 @@ export class TodosComponent implements OnInit {
   ngOnInit(): void {
     this.todos = [
       {
-        content: "do internship",
-        completed: true
+        content: "Go to Gym",
+        completed: false
       },
       {
-        content: "get job",
+        content: "10k Steps",
+        completed: false
+      },
+      {
+        content: "DSA",
         completed: false
       }
     ]
+  }
+
+  toggleDone(id: number) {
+    this.todos.map((todo, i) => {
+      if (i == id) todo.completed = !todo.completed
+      return todo
+    }
+    )
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => id != i)
+  }
+
+  addTodo() {
+    this.todos.push({ content: this.inputTodo, completed: false })
   }
 
 }
